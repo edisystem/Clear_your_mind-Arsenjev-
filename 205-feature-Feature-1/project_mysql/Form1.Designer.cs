2f75a3e62150060a0db4f021bc5f451021709ad0
Skif <edisystem@yandex.ru>
2016-10-19 11:35:36 +0300

    123

---
+++ 205-feature-Feature-1/project_mysql/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace project_mysql
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 308);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(122, 35);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "подключение";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(687, 229);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(161, 309);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(112, 33);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "новая запись";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(291, 309);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(150, 34);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "выводим посл. 10 данных";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(461, 308);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(136, 34);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "вводим строки";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(18, 266);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(66, 20);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.Text = "U menja";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(90, 266);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(66, 20);
+            this.textBox2.TabIndex = 6;
+            this.textBox2.Text = "poluchilos";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(162, 266);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(66, 20);
+            this.textBox3.TabIndex = 7;
+            this.textBox3.Text = "sdelat";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(234, 266);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(66, 20);
+            this.textBox4.TabIndex = 8;
+            this.textBox4.Text = "chto-to";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(306, 266);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(66, 20);
+            this.textBox5.TabIndex = 9;
+            this.textBox5.Text = "stojashee";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(378, 266);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(66, 20);
+            this.textBox6.TabIndex = 10;
+            this.textBox6.Text = "la-la";
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(450, 266);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(66, 20);
+            this.textBox7.TabIndex = 11;
+            this.textBox7.Text = "pa-pa";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 508);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Pilim_lepim";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+    }
+}
+
---
+++ 205-feature-Feature-1/project_mysql/Form1.cs
@@ -0,0 +1,119 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace project_mysql
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+      const  string serverName = "192.168.0.200"; // Адрес сервера (для локальной базы пишите "localhost")
+      const string userName = "student"; // Имя пользователя
+      const string dbName = "bd5k"; //Имя базы данных
+      const string port = "3306"; // Порт для подключения
+      const string password = "student"; // Пароль для подключения
+      const string connStr = "server=" + serverName +
+                ";user=" + userName +
+                ";database=" + dbName +
+                ";port=" + port +
+                ";password=" + password + ";";
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+                        /*           MySqlConnection conn = new MySqlConnection(connStr);
+                       conn.Open();
+                       string sql = "SELECT * FROM table1"; // Строка запроса
+                       MySqlScript script = new MySqlScript(conn, sql);
+                       int count = script.Execute();
+                       richTextBox1.Text += ("Executed " + count + " statement(s).");
+                       richTextBox1.Text += ("Delimiter: " + script.Delimiter);
+                       */
+
+            string sql = "SELECT * FROM vopr"; // Строка запроса
+            MySqlConnection connection = new MySqlConnection(connStr);
+            MySqlCommand sqlCom = new MySqlCommand(sql, connection);
+            connection.Open();
+            sqlCom.ExecuteNonQuery();
+            MySqlDataAdapter dataAdapter = new MySqlDataAdapter(sqlCom);
+            DataTable dt = new DataTable();
+            dataAdapter.Fill(dt);
+
+            var myData = dt.Select();
+            for (int i = 0; i < myData.Length; i++)
+            {
+                for (int j = 0; j < myData[i].ItemArray.Length; j++)
+                    richTextBox1.Text += myData[i].ItemArray[j] + "; ";
+                richTextBox1.Text += "\n";
+            }
+            connection.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string sql = "INSERT INTO vopr (id, vopr, o1, o2, o3, o4, tip, otv) VALUES (\"pitajus\", \"wpisat\", \"chto-to\", \"adekwatnoe\", \"dsf\", \"aswef\", \"1\", \"1\");"; // Строка запроса
+            MySqlConnection connection = new MySqlConnection(connStr);
+            MySqlCommand sqlCom = new MySqlCommand(sql, connection);
+            connection.Open();
+            sqlCom.ExecuteNonQuery();
+            connection.Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string sql = "SELECT * FROM `vopr` ORDER BY `id` DESC LIMIT 10 "; // Строка запроса
+            MySqlConnection connection = new MySqlConnection(connStr);
+            MySqlCommand sqlCom = new MySqlCommand(sql, connection);
+            connection.Open();
+            sqlCom.ExecuteNonQuery();
+            MySqlDataAdapter dataAdapter = new MySqlDataAdapter(sqlCom);
+            DataTable dt = new DataTable();
+            dataAdapter.Fill(dt);
+           
+            richTextBox1.Clear();
+            
+            var myData = dt.Select();
+            for (int i = 0; i < myData.Length; i++)
+            {
+                for (int j = 0; j < myData[i].ItemArray.Length; j++)
+                    richTextBox1.Text += myData[i].ItemArray[j] + "; ";
+                richTextBox1.Text += DateTime.Now+"\n";
+            }
+            connection.Close();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string vopr = textBox1.Text;
+            string o1 = textBox2.Text;
+            string o2 = textBox3.Text;
+            string o3 = textBox4.Text;
+            string o4 = textBox5.Text;
+            string tip = textBox6.Text;
+            string otv = textBox7.Text;
+
+
+            string sql = "INSERT INTO vopr (vopr, o1, o2, o3, o4, tip, otv) VALUES ('" + vopr + "', '" + o1 + "', '" + o2 + "', '" + o3 + "', '" + o4 + "', '" + tip + "', '" + otv + "')"; // Строка запроса
+            MySqlConnection connection = new MySqlConnection(connStr);
+            MySqlCommand sqlCom = new MySqlCommand(sql, connection);
+            connection.Open();
+            sqlCom.ExecuteNonQuery();
+
+            button3.PerformClick();
+
+            connection.Close();
+        }
+    }
+}
